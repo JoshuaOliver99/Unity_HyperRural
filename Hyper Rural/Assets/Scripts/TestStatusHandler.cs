770e28585ebc02e26f79d16ca8b59c5e90ff1be1
JoshuaOliver99 <56636418+JoshuaOliver99@users.noreply.github.com>
2020-02-11 19:07:19 +0000

    Proposals pull and effect a global stats

---
+++ Hyper Rural/Assets/Scripts/TestStatusHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestStatusHandler : MonoBehaviour
+{
+    public int stat1, stat2, stat3, stat4, stat5 = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("stat1: " + stat1 +
+                " stat2: " + stat2 +
+                " stat3: " + stat3 +
+                " stat4: " + stat4 +
+                " stat5: " + stat5);
+
+        }
+    }
+}
