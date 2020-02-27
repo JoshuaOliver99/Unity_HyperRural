e5ddeecba90665ec4d6d1c3a738bcbb06c3af932
JoshuaOliver99 <56636418+JoshuaOliver99@users.noreply.github.com>
2020-02-27 15:54:17 +0000

    Added to Proposals DB
    added to proposals DB

--- Hyper Rural/Assets/Scripts/CompetitionTable.cs
+++ Hyper Rural/Assets/Scripts/CompetitionTable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatusHandler : MonoBehaviour
+public class CompetitionTable : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Hyper Rural/Assets/Scripts/Stats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Stats : MonoBehaviour
+{
+    public Text Status; // For displaying stats
+    public int econemy, environment, appeal, ecoDiversity = 0;
+    void Update()
+    {
+        Status.text = ("Economy: " + econemy + " environment: " + environment + " Appeal: " + appeal +
+                " Eco-DIversity: " + ecoDiversity);
+    }
+}
