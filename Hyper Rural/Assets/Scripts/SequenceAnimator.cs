97cddcce9787933f4765a235b61a48788a67fcce
JacobLister22 <60367192+JacobLister22@users.noreply.github.com>
2020-03-05 09:54:57 +0000

    Main menu
    Added main menu
    Tested player controller in the office scene

---
+++ Hyper Rural/Assets/Scripts/SequenceAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceAnimator : MonoBehaviour
+{
+    List<Animator> _animators;
+    public float WaitBetween = 0.08f;
+    public float WaitEnd = 3f;
+    void Start()
+    {
+        _animators = new List<Animator>(GetComponentsInChildren<Animator>());
+
+        StartCoroutine(DoAnimation());
+    }
+
+    IEnumerator DoAnimation()
+    {
+        while(true)
+        {
+            foreach (var animator in _animators)
+            {
+                animator.SetTrigger("DoAnimation");
+                yield return new WaitForSeconds(WaitBetween);
+            }
+
+            yield return new WaitForSeconds(WaitEnd);
+        }
+    }
+}
