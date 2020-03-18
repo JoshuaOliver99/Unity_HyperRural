0ac36ce7987897ec9ab826d25e07d6148e1881c1
JoshuaOliver99 <56636418+JoshuaOliver99@users.noreply.github.com>
2020-03-18 02:28:41 +0000

    Added visual reflection of stats and Updated ChartValues.cs
    +> Added the StatsCanvas prefab into the office scene
    +> Neatened ChartValues.cs and commented the functions of code

--- Hyper Rural/Assets/Scripts/ChartValues.cs
+++ Hyper Rural/Assets/Scripts/ChartValues.cs
@@ -1,50 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-
 
 public class ChartValues : MonoBehaviour
 {
-    public Image ecoChart;
-    public Image envirChart;
-    public Image appChart;
-    public Image ecoDChart;
+    [Header("References")]
+    [SerializeField] Image ecoChart; // Links to images of charts
+    [SerializeField] Image envirChart;
+    [SerializeField] Image appChart;
+    [SerializeField] Image ecoDChart;
 
     void Update()
     {
-        ecoChart.fillAmount = (float)GameController.economy / 10;
+        ecoChart.fillAmount = (float)GameController.economy / 10; // Int value of stats conveted to float, applied to fill amount (0-1) 
         envirChart.fillAmount = (float)GameController.environment / 10;
         appChart.fillAmount = (float)GameController.appeal / 10;
         ecoDChart.fillAmount = (float)GameController.ecoDiversity / 10;
 
-        if (GameController.economy < 5)
-            ecoChart.color = new Color(255, 0, 0);
-        else if (GameController.economy > 10)
-            ecoChart.color = new Color(0, 255, 0);
+        if (GameController.economy < 3) // If (economy is low)
+            ecoChart.color = new Color(255, 0, 0); // Colour bar red
+        else if (GameController.economy > 8) // If (economy is heigh)
+            ecoChart.color = new Color(0, 255, 0); // Colour bar green
         else
-            ecoChart.color = new Color(0, 0, 0);
+            ecoChart.color = new Color(0, 0, 0); // else, black
 
-        if (GameController.environment < 5)
+        if (GameController.environment < 3)
             envirChart.color = new Color(255, 0, 0);
-        else if (GameController.environment > 10)
+        else if (GameController.environment > 8)
             envirChart.color = new Color(0, 255, 0);
         else
             envirChart.color = new Color(0, 0, 0);
 
-        if (GameController.appeal < 5)
+        if (GameController.appeal < 3)
             appChart.color = new Color(255, 0, 0);
-        else if (GameController.appeal > 10)
+        else if (GameController.appeal > 8)
             appChart.color = new Color(0, 255, 0);
         else
             appChart.color = new Color(0, 0, 0);
 
-        if (GameController.ecoDiversity < 5)
+        if (GameController.ecoDiversity < 3)
             ecoDChart.color = new Color(255, 0, 0);
-        else if (GameController.ecoDiversity > 10)
+        else if (GameController.ecoDiversity > 8)
             ecoDChart.color = new Color(0, 255, 0);
         else
             ecoDChart.color = new Color(0, 0, 0);
-
     }
 }
